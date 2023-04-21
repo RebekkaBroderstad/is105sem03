df1153d67c3b787dce7812c61dd3c25fe448b652
RebekkaBroderstad <rebekkab@uia.no>
2023-04-21 09:35:41 +0200

    endret mycrypt

--- mycrypt/mycrypt_test.go
+++ mycrypt/mycrypt_test.go
@@ -1,28 +1,47 @@
 package mycrypt
 
-var ALF_SEM03 []rune = []rune("abcdefghijklmnopqrstuvwxyzæøå0123456789.,:; ")
+import (
+	"reflect"
+	"testing"
+)
 
-func Krypter(melding []rune, alphabet []rune, chiffer int) []rune {
-        kryptertMelding := make([]rune, len(melding))
-        for i, symbol := range melding {
-                indeks := sokIAlfabetet(symbol, alphabet)
-                if indeks == -1 {
-                        kryptertMelding[i] = symbol
-                } else if indeks+chiffer >= len(alphabet) {
-                        kryptertMelding[i] = alphabet[indeks+chiffer-len(alphabet)]
-                } else {
-                        kryptertMelding[i] = alphabet[indeks+chiffer]
-                }
-        }
-        return kryptertMelding
+// Testene forutsetter et alfabet, som er definert i mycrypt.go som ALF_SEM03
+
+func TestKrypter(t *testing.T) {
+
+	type test struct {
+		inputMessage []rune
+		chiffer      int
+		want         []rune
+	}
+	tests := []test{
+		{inputMessage: []rune("w"), chiffer: 4, want: []rune("æ")},
+		{inputMessage: []rune("0"), chiffer: 4, want: []rune("4")},
+	}
+
+	for _, tc := range tests {
+		got := Krypter(tc.inputMessage, ALF_SEM03, tc.chiffer)
+		if !reflect.DeepEqual(got, tc.want) {
+			t.Errorf("Feil, for chiffer %d, fikk %q, ønsket %q.", tc.chiffer, got, tc.want)
+		}
+	}
 }
 
-func sokIAlfabetet(symbol rune, alfabet []rune) int {
-        for i, s := range alfabet {
-                if s == symbol {
-                        return i
-                }
-        }
-        return -1
-}
+// Posisjonene i alfabetet begynner på 0 fra venstre og teller oppover mot høyre
+func TestSokIAlfabetet(t *testing.T) {
+	type test struct {
+		input rune
+		want  int
+	}
+	tests := []test{
+		{input: 'æ', want: 26},
+		{input: 'a', want: 0},
+	}
 
+	for _, tc := range tests {
+		got := sokIAlfabetet(tc.input, ALF_SEM03)
+		if got != tc.want {
+			t.Errorf("Feil, fikk %d, ønsket %d.", got, tc.want)
+		}
+	}
+}
