0b6d66c681ac5b13f013ad67bc990697e93758f8
RebekkaBroderstad <rebekkab@uia.no>
2023-04-21 09:26:18 +0200

    endret mycrypt

--- mycrypt/mycrypt_test.go
+++ mycrypt/mycrypt_test.go
@@ -1,49 +1,28 @@
 package mycrypt
 
-import (
-	"reflect"
-	"testing"
-)
+var ALF_SEM03 []rune = []rune("abcdefghijklmnopqrstuvwxyzæøå0123456789.,:; ")
 
-// Testene forutsetter et alfabet, som er definert i mycrypt.go som ALF_SEM03
-
-func TestKrypter(t *testing.T) {
-
-	type test struct {
-		inputMessage []rune
-                chiffer int
-		want  []rune
-	}
-	tests := []test{
-		{inputMessage: []rune("w"), chiffer: 4, want: []rune("æ")},
-		{inputMessage: []rune("0"), chiffer: 4, want: []rune("4")},
-		{inputMessage: []rune("Kjevik;SN39040;18.03.2022 01:50;6"), chiffer: 4, want: []rune("dnizmocdd7;484c5: 47 6466d45b94c.")},
-                {inputMessage: []rune("dnizmocdd7;484c5: 47 6466d45b94c."), chiffer: len(ALF_SEM03) - 4, want: []rune("Kjevik;SN39040;18.03.2022 01:50;6")},
-	}
-
-	for _, tc := range tests {
-		got := Krypter(tc.inputMessage, ALF_SEM03, tc.chiffer)
-		if !reflect.DeepEqual(got, tc.want) {
-			t.Errorf("Feil, for chiffer %d, fikk %q, ønsket %q.", tc.chiffer, got, tc.want)
-		}
-	}
+func Krypter(melding []rune, alphabet []rune, chiffer int) []rune {
+        kryptertMelding := make([]rune, len(melding))
+        for i, symbol := range melding {
+                indeks := sokIAlfabetet(symbol, alphabet)
+                if indeks == -1 {
+                        kryptertMelding[i] = symbol
+                } else if indeks+chiffer >= len(alphabet) {
+                        kryptertMelding[i] = alphabet[indeks+chiffer-len(alphabet)]
+                } else {
+                        kryptertMelding[i] = alphabet[indeks+chiffer]
+                }
+        }
+        return kryptertMelding
 }
 
-// Posisjonene i alfabetet begynner på 0 fra venstre og teller oppover mot høyre
-func TestSokIAlfabetet(t *testing.T) {
-	type test struct {
-		input rune
-		want  int
-	}
-	tests := []test{
-		{input: 'æ', want: 26},
-		{input: 'a', want: 0},
-	}
-
-	for _, tc := range tests {
-		got := sokIAlfabetet(tc.input, ALF_SEM03)
-		if got != tc.want {
-			t.Errorf("Feil, fikk %d, ønsket %d.", got, tc.want)
-		}
-	}
+func sokIAlfabetet(symbol rune, alfabet []rune) int {
+        for i, s := range alfabet {
+                if s == symbol {
+                        return i
+                }
+        }
+        return -1
 }
+
